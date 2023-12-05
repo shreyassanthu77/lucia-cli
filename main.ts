88cce1a15fd29413e332423c4b6eca2429278f9b
Shreyas Mididoddi <me@shreyascodes.tech>
2023-12-05 19:10:40 +0530

    feat: create a basic cli

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-console.log("Lucia is the best!");
+import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+
+const initCommand = new Command()
+  .name("init")
+  .description("Adds Lucia Auth to your project")
+  .action(() => {
+    console.log("Lucia Auth has been added to your project!");
+  });
+
+const root = new Command()
+  .name("lucia")
+  .version("0.0.1")
+  .description("A CLI for Lucia Auth")
+  .action(() => {
+    root.showHelp();
+  })
+  .command("init", initCommand);
+
+await root.parse(Deno.args);
